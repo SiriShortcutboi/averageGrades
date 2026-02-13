2109596edd8b6d1cc3aced19cc0104e83283251d
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-02-12 20:14:34 -0700

    added decimal support with a double, not a long

--- averageGrades/Program.cs
+++ averageGrades/Program.cs
@@ -4,18 +4,21 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine("How many grades will you enter?");
-        int size = int.Parse(Console.ReadLine());
+		Console.WriteLine("How many grades do you want to give to the machine?");
+        int arraySize = int.Parse(Console.ReadLine());
 
         //declare a new int array at the size of user's number
-        int[] grades = new int[size];
+        int[] grades = new int[arraySize];
         int sum = 0;
-        for (int i = 0; i < size; i++)
+        double deciSum = 0;
+        for (int i = 0; i < arraySize; i++)
         {
-            Console.WriteLine("Enter grade " + (i + 1) + ":");
+            Console.WriteLine("Enter your grade number " + (i + 1));
             grades[i] = int.Parse(Console.ReadLine());
             sum += grades[i];
         }
-        Console.WriteLine(sum/size);
+        deciSum = sum;
+        string gradeTotal = (deciSum/arraySize).ToString();
+        Console.WriteLine("Your grade average is " + gradeTotal);
     }
 }
